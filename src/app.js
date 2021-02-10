c682ef3fad97538860e2f8a261826ce33c20bea9
Akash <akashrajpoots14@gmail.com>
2021-02-10 00:28:10 -1200

    changes after commits

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.get('',(req,res)=>{
     res.send("HI THIS IS AN WHEATHER APP");
 })
 app.get('/weather',(req,res)=>{
-    res.send("this is wheather end point");
+    res.send("this is wheather end");
 })
 app.get('*',(req,res)=>{
     res.send("Page not found");
