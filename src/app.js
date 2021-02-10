1b17f9df3b75062b1d847f2036f60dc17782be4c
Akash <akashrajpoots14@gmail.com>
2021-02-09 23:50:52 -1200

    config tab and Apis

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('',(req,res)=>{
+    res.send("HI THIS IS AN WHEATHER APP");
+})
+app.get('/weather',(req,res)=>{
+    res.send("this is wheather end point");
+})
+app.get('*',(req,res)=>{
+    res.send("Page not found");
+})
+
 app.listen(port,()=>{
     console.log("Server is Running  on port",port);
-})
+})             
