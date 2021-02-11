150bf7febdfef5f0ac639628ad544737286db9fc
Akash <akashrajpoots14@gmail.com>
2021-02-10 19:29:27 -1200

    Cofigration and  creating in weather data

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL:"http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"6d557b3967b935a8e43071a52e92ad9a"
+    }
+}
+module.exports = constants;
