2e8b0199d2ef1a4ffc309109c2cfe8ab22c2fea5
Akash <akashrajpoots14@gmail.com>
2021-02-10 23:23:40 -1200

    Added css partials hbs

--- src/app.js
+++ src/app.js
@@ -7,21 +7,29 @@ const weatherData = require('../utils/weatherData');
 
 const publicStaticDirPath = path.join(__dirname,'../public'); 
 const partialsPath = path.join(__dirname,'../templates/partials');
-const viewPath = path.join(__dirname,'../templates/views');
+const viewPaths = path.join(__dirname,'../templates/views');
 
-app.set('view engine',hbs);
-app.set('views',viewPath);
+app.set('view engine','hbs');
+app.set('views',viewPaths);
+hbs.registerPartials(partialsPath);
 app.use(express.static(publicStaticDirPath));
 
 
 const port = process.env.PORT || 3000;
 
 app.get('',(req,res)=>{
-    res.send("HI THIS IS AN WHEATHER APP");
+    res.render('index',{
+        title:'Weather App'
+    })
 })
 //localhost:3000/weather?address=itarsi
 app.get('/weather',(req,res)=>{
     const address = req.query.address;
+    if(!address){
+        return res.send({
+            error : "You must enter the address in search box "
+        })
+    }
 
     weatherData(address,(error,{temprature,description,cityName})=>{
         if(error){
